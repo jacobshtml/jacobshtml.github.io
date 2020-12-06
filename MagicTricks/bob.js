4714d26541bbe3c8253d1df20ad0d0ba67b24987
jacobshtml <68833854+jacobshtml@users.noreply.github.com>
2020-12-06 15:47:11 -0800

    Update bob.js

--- MagicTricks/bob.js
+++ MagicTricks/bob.js
@@ -1,9 +1,11 @@
-var element = document.getElementById("big-small");
 $( document ).ready(function() {
-	element.classList.add("big");
+	
+  $( "#big-small" ).addClass( "big" );
 	setTimeout(function() {
-  element.classList.add("rotate")
+  $( "#big-small" ).addClass( "rotate" );
   }, 2000);
   setTimeout(function() {
-  element.classList.add("small");}, 3000);
+  
+  $( "#big-small" ).addClass( "small" );
+  }, 3000);
 });
